46290ad7a7e48e9129569d991668979385a55106
Enes DEMİR <enesdem175@gmail.com>
2024-12-23 09:32:58 +0300

    Update

--- src/inputs/create-user-dto.input.ts
+++ src/inputs/create-user-dto.input.ts
@@ -3,6 +3,5 @@
 export class CreateUserDto {
     name: string;
     email: string;
-    // İsteğe bağlı olarak password, telefon vb. ek alanlar...
   }
   
--- src/inputs/get-areas-dto.input.ts
+++ src/inputs/get-areas-dto.input.ts
@@ -3,13 +3,13 @@ import { IsOptional, IsInt, Min } from 'class-validator';
 
 export class GetAreasDto {
   @IsOptional()
-  @Transform(({ value }) => parseInt(value, 10)) // Gelen değeri sayıya dönüştür
+  @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   @Min(1)
   limit: number = 20;
 
   @IsOptional()
-  @Transform(({ value }) => parseInt(value, 10)) // Gelen değeri sayıya dönüştür
+  @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   @Min(1)
   page: number = 1;
